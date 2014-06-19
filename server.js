41f62d30fd95345d538a26cd985f7e6b433f9428
Clay Hollenback <clay.hollenback@gmail.com>
2014-06-19 13:49:35 -0500

    added delete

--- server.js
+++ server.js
@@ -6,46 +6,64 @@ console.log(__dirname)
 app.use(express.static(__dirname + '/client'));
 
 app.get('/', function(req, res) {
-    fs.readFile('index.html', function (err, data) {
-  		if (err) throw err;
+    fs.readFile('index.html', function(err, data) {
+        if (err) throw err;
 
-  		res.send(data.toString());
-	});
+        res.send(data.toString());
+    });
 });
 
-app.get ('/test', function (req, res){
-  		var clay = {
-  			"title": "my dummy to do item",
-  			"completed": false,
-  			"id": "7590287345098237459087"
-  		}
-	res.json (clay);
+app.get('/test', function(req, res) {
+    var clay = {
+        "title": "my dummy to do item",
+        "completed": false,
+        "id": "7590287345098237459087"
+    }
+    res.json(clay);
 });
 
-app.get ('/items', function (req, res){
-  fs.readFile('data.json', function (err, data) {
-    if (err) throw err;
-    var dataitems=JSON.parse (data.toString());
-    res.json (dataitems);
-  });
+app.get('/items', function(req, res) {
+    fs.readFile('data.json', function(err, data) {
+        if (err) throw err;
+        var dataitems = JSON.parse(data.toString());
+        res.json(dataitems);
+    });
 
 
 });
-  
-app.get ('/items/:id', function (req, res){
-  fs.readFile('data.json', function (err, data) {
-    if (err) throw err;
-    var dataitems=JSON.parse (data.toString());
-    for (var i = 0; i < dataitems.length; i++) {
-      if (dataitems[i].id === req.params.id) {
-        res.json(dataitems[i]);
-        return  
-      }
-    }; 
-    res.send(404)
-  }); 
+
+app.get('/items/:id', function(req, res) {
+    fs.readFile('data.json', function(err, data) {
+        if (err) throw err;
+        var dataitems = JSON.parse(data.toString());
+        for (var i = 0; i < dataitems.length; i++) {
+            if (dataitems[i].id === req.params.id) {
+                res.json(dataitems[i]);
+                return
+            }
+        };
+        res.send(404)
+    });
 });
- 
+
+app.delete('/items/:id', function(req, res) {
+    fs.readFile('data.json', function(err, data) {
+        if (err) throw err;
+        var dataitems = JSON.parse(data.toString());
+        for (var i = 0; i < dataitems.length; i++) {
+            if (dataitems[i].id === req.params.id) {
+                dataitems.splice(i, 1);
+                var s = JSON.stringify(dataitems);
+                fs.writeFile('data.json', s, function() {
+                    res.send('');
+                });
+                return
+            }
+        };
+        res.send(404)
+    });
+});
+
 console.log("Starting to listen");
 
 var server = app.listen(3000, function() {
