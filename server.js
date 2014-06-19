282b4852d913173dba192876b89360fb00800b0b
Aaron Seegmiller <seegmiller.aaron@gmail.com>
2014-06-19 14:32:17 -0500

    added post functionality

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 
 console.log(__dirname)
 app.use(express.static(__dirname + '/client'));
+app.use(express.bodyParser());
 
 app.get('/', function(req, res) {
     fs.readFile('index.html', function(err, data) {
@@ -61,6 +62,34 @@ app.delete('/items/:id', function(req, res) {
             }
         };
         res.send(404)
+    });
+});
+
+function createGuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+    });
+}
+
+
+app.post('/items', function(req, res){
+    console.log("post");
+    console.log(req.body.title);
+    var newItem = {
+        "title":req.body.title,
+        "completed":false,
+        "id": createGuid()
+    };
+    console.log(newItem);
+    fs.readFile('data.json', function(err, data) {
+        if (err) throw err;
+        var dataitems = JSON.parse(data.toString());
+        dataitems.push(newItem);
+        var s = JSON.stringify(dataitems);
+        fs.writeFile('data.json', s, function() {
+            res.send('');
+        });
     });
 });
 
